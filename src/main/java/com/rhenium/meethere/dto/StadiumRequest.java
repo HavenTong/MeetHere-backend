1565bd0d22cddabace8529631cb8d4c2368360c1
mufanx <35735090+mufanx@users.noreply.github.com>
2019-12-16 23:44:30 +0800

    完成StadiumRequest类，完成Stadium类型的转换（使用枚举）

---
+++ src/main/java/com/rhenium/meethere/dto/StadiumRequest.java
@@ -0,0 +1,23 @@
+package com.rhenium.meethere.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author YueChen
+ * @version 1.0
+ * @date 2019/12/16 23:06
+ */
+@Data
+@AllArgsConstructor
+@Builder
+public class StadiumRequest {
+    private Integer stadiumId;
+    private String stadiumName;
+    private Integer type;
+    private String location;
+    private String description;
+    private double price;
+    private String picture;
+}

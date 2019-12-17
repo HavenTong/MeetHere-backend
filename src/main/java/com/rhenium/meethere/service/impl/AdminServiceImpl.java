38052c2f957a3dd16fc281c889adcf552d7c900c
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-12-17 13:53:10 +0800

    Update AdminServiceImpl.java

--- src/main/java/com/rhenium/meethere/service/impl/AdminServiceImpl.java
+++ src/main/java/com/rhenium/meethere/service/impl/AdminServiceImpl.java
@@ -2,12 +2,17 @@ package com.rhenium.meethere.service.impl;
 
 import com.rhenium.meethere.dao.CustomerDao;
 import com.rhenium.meethere.service.AdminService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author JJAYCHEN
  * @date 2019/12/17 1:07 下午
  */
+
+@Slf4j
+@Service
 public class AdminServiceImpl implements AdminService {
     @Autowired
     private CustomerDao customerDao;

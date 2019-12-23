b558041dd1dacb9275c92e1e38e2d9408de663da
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-12-23 14:50:21 +0800

    Update Stadium.java
    添加 Builder 注解

--- src/main/java/com/rhenium/meethere/domain/Stadium.java
+++ src/main/java/com/rhenium/meethere/domain/Stadium.java
@@ -1,6 +1,7 @@
 package com.rhenium.meethere.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Stadium implements Serializable {
     private static final long serialVersionUID = -4454785377509579955L;
 
